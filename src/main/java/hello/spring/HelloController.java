6dfb595827839afaf77bcd485d8e5a65b9690767
reako99 <zldrgx@naver.com>
2024-02-23 18:16:05 +0900

    update annotation

--- src/main/java/hello/spring/HelloController.java
+++ src/main/java/hello/spring/HelloController.java
@@ -1,7 +1,9 @@
 package hello.spring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
     @GetMapping("/hello-spring")
