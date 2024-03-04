7c4bc40e0b40bd33367dd7c9688415f13493f424
reako99 <zldrgx@naver.com>
2024-03-04 18:11:09 +0900

    add ExternalApplication

--- src/main/java/hello/ExternalApplication.java
+++ src/main/java/hello/ExternalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectV2Application {
+public class ExternalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectV2Application.class, args);
+        SpringApplication.run(ExternalApplication.class, args);
     }
 
 }
