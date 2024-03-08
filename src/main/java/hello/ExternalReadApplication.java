cc791fd08ffdbd819426ba66d4f7accbc02bd08d
reako99 <zldrgx@naver.com>
2024-03-08 18:55:02 +0900

    change to external-read

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExternalApplication {
+public class ExternalReadApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExternalApplication.class, args);
+        SpringApplication.run(ExternalReadApplication.class, args);
     }
 
 }
